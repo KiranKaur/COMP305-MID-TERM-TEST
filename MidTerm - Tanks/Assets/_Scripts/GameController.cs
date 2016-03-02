c2432ce65b7c196e0d512d23626bc8e5e99e4215
KiranKaur <k.angel117@yahoo.com>
2016-03-02 18:49:33 -0500

    Add Scripts

---
+++ MidTerm - Tanks/Assets/_Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public int tankNumber=4;
+    public EnemyController tank;
+    //public GameObject tank;
+	
+	// Use this for initialization
+	void Start () {
+		this._GenerateTanks ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// generate Clouds
+	private void _GenerateTanks() {
+        for (int tankCount = 0; tankCount < this.tankNumber; tankCount++)
+            Instantiate(tank.gameObject);
+    }
+	}
+
